791b070843b7bf345cc7b65f2bef5fd8313c8695
Corey McCue <coreysmccue@gmail.com>
2021-10-05 15:45:21 +0200

    link testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <Card style={{ width: '18rem' }}>
       <ListGroup variant="flush">
-      <Card.Link href="www.google.com">Card Link</Card.Link>
+      <Card.Link href="https://music.apple.com/us/artist/waiting-for-smith/1192026265">Card Link</Card.Link>
         <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
         <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
       </ListGroup>
