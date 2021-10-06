9a187352a4ffbcd7616f1fe67e1a223527d06ee8
Corey McCue <coreysmccue@gmail.com>
2021-10-06 08:12:57 +0200

    add insta

--- src/Components/Instagram.js
+++ src/Components/Instagram.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import logo from './photos/instagram.svg';
+import '../../src/App.css';
 
 function Instagram() {
   return (
-    <div>
-      <a href="https://www.instagram.com/waitingforsmith/">Instagram</a>
+    <div className="content-container">
+      <img src={logo} alt="" className="insta-image" />
+      <Button href="https://open.spotify.com/artist/57oieIojgviKMyTtzG4kn2?si=rjZkfDosS3WLFU5nPM2rwA&dl_branch=1" variant="outline-secondary">Follow</Button>
+      {/* <a href="https://www.instagram.com/waitingforsmith/">Instagram</a> */}
     </div>
   )
 }
