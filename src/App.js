843dd36c41717d4e9b121cb751ef80db2b3aba8a
Corey McCue <coreysmccue@gmail.com>
2021-10-06 07:10:05 +0200

    add logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Image} from 'react';
+import React from 'react';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import './App.css';
 import Itunes from './Components/Itunes';
@@ -7,33 +7,41 @@ import Instagram from './Components/Instagram';
 import Youtube from './Components/Youtube';
 import Deezer from './Components/Deezer';
 import Website from './Components/Website';
+import Hopeless from './Components/Hopeless';
 import pic from './Portret-1059.jpg'
+import logo from './logoWFS.jpg'
 
 function App() {
   return (
-    <Card style={{ width: '18rem' }} className="entire-container card">
-      <img src={pic} alt="Harry waiting for smith" className="picture" />
-      <ListGroup variant="flush">
-        <ListGroupItem>
-          <Itunes />
-        </ListGroupItem>
-        <ListGroup.Item>
-          <Spotify />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Instagram />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Youtube />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Deezer />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Website />
-        </ListGroup.Item>
-      </ListGroup>
-    </Card>
+    <>
+      <Card style={{ width: '18rem' }} className="entire-container card">
+      <img src={logo} alt="Harry waiting for smith" className="logo" />
+        <img src={pic} alt="Harry waiting for smith" className="picture" />
+        <ListGroup variant="flush">
+          <ListGroupItem>
+            <Itunes />
+          </ListGroupItem>
+          <ListGroup.Item>
+            <Spotify />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Instagram />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Youtube />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Deezer />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Hopeless />
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Website />
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
+    </>
   )
 }
 
---
+++ src/Components/Hopeless.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hopeless() {
+  return (
+    <div>
+      <a href="https://music.apple.com/gb/album/hopelessness-of-love-ep/1569151487">Hopelessness of Love</a>
+    </div>
+  )
+}
+
+export default Hopeless
