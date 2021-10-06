33b8e7318ba891e454535e55bd470a486ad70b34
Corey McCue <coreysmccue@gmail.com>
2021-10-06 22:32:07 +0200

    add website and pic test

--- src/Components/Website.js
+++ src/Components/Website.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import logo from './photos/web.jpeg';
+import '../../src/App.css';
 
 function Website() {
   return (
-    <div>
-      <a href="https://www.waitingforsmith.com/">Website</a>
+    <div className="content-container">
+      <img src={logo} alt="" className="spotify-image" />
+      <Button href="https://www.waitingforsmith.com/" variant="outline-secondary">Visit</Button>
+      {/* <a href="https://www.waitingforsmith.com/">Website</a> */}
     </div>
   )
 }
