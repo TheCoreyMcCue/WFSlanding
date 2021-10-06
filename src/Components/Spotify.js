5e79dae4bb8031120b1d14ea26353953e80cb090
Corey McCue <coreysmccue@gmail.com>
2021-10-06 07:47:01 +0200

    figured out button

--- src/Components/Spotify.js
+++ src/Components/Spotify.js
@@ -7,7 +7,7 @@ function Spotify() {
   return (
     <div className="content-container">
       <img src={logo} alt="" className="spotify-image" />
-      <Button href="https://open.spotify.com/artist/57oieIojgviKMyTtzG4kn2?si=rjZkfDosS3WLFU5nPM2rwA&dl_branch=1" variant="secondary">Play</Button>
+      <Button href="https://open.spotify.com/artist/57oieIojgviKMyTtzG4kn2?si=rjZkfDosS3WLFU5nPM2rwA&dl_branch=1" variant="outline-secondary">Play</Button>
     </div>
   )
 }
