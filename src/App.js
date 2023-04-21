9399a36337b9c384d54334cae01db3694604bf34
Corey McCue <corey.mccue@sky.uk>
2023-04-21 15:24:07 +0200

    under construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import logo from './harry-hands.jpeg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>This site is currently under construction and will be back soon.</p><p>  In the meantime</p>
+				<a
+					className='App-link'
+					href='https://www.waitingforsmith.co.uk/'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					please visit our website
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
