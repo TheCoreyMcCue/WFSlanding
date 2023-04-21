689ef4d309316180185b79ba210a5e2bbf3a4a96
Corey McCue <corey.mccue@sky.uk>
2023-04-21 15:04:52 +0200

    reboot

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import logo from './logo.svg';
 import './App.css';
-import Itunes from './Components/Itunes';
-import Spotify from './Components/Spotify';
-import Instagram from './Components/Instagram';
-import Youtube from './Components/Youtube';
-import Deezer from './Components/Deezer';
-import Website from './Components/Website';
-import Hopeless from './Components/Hopeless';
-import pic from './Square-potret.jpg';
-import logo from './logo-wfs.png';
 
 function App() {
-	return (
-		<div className='site x'>
-			<Card style={{ width: '18rem' }} className='entire-container card'>
-				<img src={logo} alt='Harry waiting for smith' className='logo' />
-				<div className='border-div'>
-					<img src={pic} alt='Harry waiting for smith' className='picture' />
-					<ListGroup variant='flush'>
-						<ListGroup.Item>
-							<Spotify />
-						</ListGroup.Item>
-						<ListGroupItem>
-							<Itunes />
-						</ListGroupItem>
-						<ListGroup.Item>
-							<Youtube />
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Deezer />
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Hopeless />
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Instagram />
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<Website />
-						</ListGroup.Item>
-					</ListGroup>
-				</div>
-			</Card>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
