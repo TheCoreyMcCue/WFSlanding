329de11f9d7f792cb970afbb35e9ad47cec2049b
Corey McCue <corey.mccue@sky.uk>
2023-04-21 14:08:05 +0200

    redeploy

--- src/App.js
+++ src/App.js
@@ -8,43 +8,43 @@ import Youtube from './Components/Youtube';
 import Deezer from './Components/Deezer';
 import Website from './Components/Website';
 import Hopeless from './Components/Hopeless';
-import pic from './Square-potret.jpg'
+import pic from './Square-potret.jpg';
 import logo from './logo-wfs.png';
 
 function App() {
-  return (
-    <div className="site">
-      <Card style={{ width: '18rem' }} className="entire-container card">
-        <img src={logo} alt="Harry waiting for smith" className="logo" />
-        <div className="border-div">
-        <img src={pic} alt="Harry waiting for smith" className="picture" />
-        <ListGroup variant="flush">
-          <ListGroup.Item>
-            <Spotify />
-          </ListGroup.Item>
-          <ListGroupItem>
-            <Itunes />
-          </ListGroupItem>
-          <ListGroup.Item>
-            <Youtube />
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Deezer />
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Hopeless />
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Instagram />
-          </ListGroup.Item>
-          <ListGroup.Item>
-            <Website />
-          </ListGroup.Item>
-        </ListGroup>
-        </div>
-      </Card>
-    </div>
-  )
+	return (
+		<div className='site x'>
+			<Card style={{ width: '18rem' }} className='entire-container card'>
+				<img src={logo} alt='Harry waiting for smith' className='logo' />
+				<div className='border-div'>
+					<img src={pic} alt='Harry waiting for smith' className='picture' />
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<Spotify />
+						</ListGroup.Item>
+						<ListGroupItem>
+							<Itunes />
+						</ListGroupItem>
+						<ListGroup.Item>
+							<Youtube />
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Deezer />
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Hopeless />
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Instagram />
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Website />
+						</ListGroup.Item>
+					</ListGroup>
+				</div>
+			</Card>
+		</div>
+	);
 }
 
-export default App
+export default App;
