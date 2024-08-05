3043f970ff2cb407decda9221ac50c2dcf4d3b39
Corey McCue <coreysmccue@gmail.com>
2024-08-05 21:41:07 +0200

    update components

--- src/App.js
+++ src/App.js
@@ -1,76 +1,78 @@
-import Social from './components/Social';
+import Social from "./components/Social";
 
-import insta from './assets/instagram.jpeg';
-import web from './assets/web.jpeg';
-import harry from './assets/harry.png';
+import insta from "./assets/instagram.jpeg";
+import web from "./assets/web.jpeg";
+import harry from "./assets/harry.png";
 
 function App() {
-	return (
-		<section
-			className='flex justify-center backdrop-blur'
-			style={{
-				backgroundImage: `url("https://img.freepik.com/premium-photo/grey-blur-background_76542-225.jpg?w=2000")`,
-			}}
-		>
-			<div
-				id='container'
-				className='flex flex-col content-center max-w-xs h-auto rounded-b-lg my-6 shadow-2xl'
-			>
-				<img
-					id='top-image'
-					className='w-full h-auto rounded-t-lg object-cover'
-					src={harry}
-					alt=''
-				/>
+  return (
+    <section
+      className="flex justify-center items-center min-h-screen bg-cover bg-center backdrop-blur-md p-4"
+      style={{
+        backgroundImage: `url("https://img.freepik.com/premium-photo/grey-blur-background_76542-225.jpg?w=2000")`,
+      }}
+    >
+      <div
+        id="container"
+        className="flex flex-col items-center max-w-md w-full bg-white rounded-lg shadow-lg overflow-hidden"
+      >
+        <img
+          id="top-image"
+          className="w-full h-60 object-cover"
+          src={harry}
+          alt="Artist"
+        />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--e_GXTT_B--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_spotify.png'
-					link='https://open.spotify.com/artist/57oieIojgviKMyTtzG4kn2'
-				/>
+        <div className="w-full">
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--e_GXTT_B--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_spotify.png"
+            link="https://open.spotify.com/artist/57oieIojgviKMyTtzG4kn2"
+          />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--LpZFcfe0--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_applemusic_listen.png'
-					link='https://music.apple.com/nl/artist/waiting-for-smith/1192026265'
-				/>
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--LpZFcfe0--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_applemusic_listen.png"
+            link="https://music.apple.com/nl/artist/waiting-for-smith/1192026265"
+          />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--BuOsZiLg--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_deezer.png'
-					link='https://www.deezer.com/us/artist/11654939'
-				/>
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--BuOsZiLg--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_deezer.png"
+            link="https://www.deezer.com/us/artist/11654939"
+          />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--uf3wpRWG--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_amazon.png'
-					link='https://music.amazon.com/artists/B01MS44PHF/waiting-for-smith'
-				/>
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--uf3wpRWG--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_amazon.png"
+            link="https://music.amazon.com/artists/B01MS44PHF/waiting-for-smith"
+          />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--wJHSivtl--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_tidal.png'
-					link='https://tidal.com/browse/artist/8406140'
-				/>
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--wJHSivtl--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_tidal.png"
+            link="https://tidal.com/browse/artist/8406140"
+          />
 
-				<Social
-					icon='https://cloudinary-cdn.ffm.to/s--abCrNs3k--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_pandora.png'
-					link='https://api.ffm.to/sl/e/c/hopelessness-of-love?cd=eyJ1YSI6eyJ1YSI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXIiOnsibmFtZSI6IkNocm9tZSIsInZlcnNpb24iOiIxMTIuMC4wLjAiLCJtYWpvciI6IjExMiJ9LCJlbmdpbmUiOnsibmFtZSI6IkJsaW5rIiwidmVyc2lvbiI6IjExMi4wLjAuMCJ9LCJvcyI6eyJuYW1lIjoiTWFjIE9TIiwidmVyc2lvbiI6IjEwLjE1LjcifSwiZGV2aWNlIjp7fSwiY3B1Ijp7fX0sImNsaWVudCI6eyJyaWQiOiJkMmFhYTY0ZC04ZWE4LTRhNzQtYmQ4YS01Mjg3Mzg2NWI0MWYiLCJzaWQiOiJiMTg5MTVkMi00N2E4LTQ1ZDUtOGY1MS0yMWQ2MmRiOTZmZjgiLCJpcCI6IjE3OC44NS42Ni4xNTMiLCJyZWYiOiJodHRwczovL2wuZmFjZWJvb2suY29tLyIsImhvc3QiOiJkaXR0by5mbSIsImxhbmciOiJlbi1VUyIsImlwQ291bnRyeSI6Ik5MIn0sImlzV2VicFN1cHBvcnRlZCI6dHJ1ZSwiaXNGcm9tRVUiOnRydWUsImNvdW50cnlDb2RlIjoiTkwiLCJ1c2VBZmYiOiJvcmlnaW4iLCJpZCI6IjYwNzBmOGQxNDUwMDAwMTMwMDU2MGQ0ZiIsInBydiI6ZmFsc2UsImlzUHJlUiI6ZmFsc2UsInR6byI6bnVsbCwiY2giOm51bGwsImFuIjpudWxsLCJkZXN0VXJsIjoiaHR0cHM6Ly9wYW5kb3JhLmFwcC5saW5rLz8kZGVza3RvcF91cmw9aHR0cHMlM0ElMkYlMkZ3d3cucGFuZG9yYS5jb20lMkZhcnRpc3QlMkZ3YWl0aW5nLWZvci1zbWl0aCUyRmxpbmVzLW9mLWxvdmUtc2luZ2xlJTJGQUw1S2xmaDduNzUyNHBjJiRpb3NfZGVlcGxpbmtfcGF0aD1wYW5kb3JhdjQlM0ElMkYlMkZiYWNrc3RhZ2UlMkZhbGJ1bSUzRnRva2VuJTNEQUwlM0E1NTg5MDA2JiRhbmRyb2lkX2RlZXBsaW5rX3BhdGg9cGFuZG9yYXY0JTNBJTJGJTJGYmFja3N0YWdlJTJGYWxidW0lM0Z0b2tlbiUzREFMJTNBNTU4OTAwNiZ-Y2hhbm5lbD1QYXJ0bmVyJTIwQ2F0YWxvZyUyMFNlYXJjaCUyMEFQSSIsInZpZCI6ImI1YWFkMDJjLTM1NmMtNDMwYi1iZDY4LWJhMTdjODUyZGU3MSIsInNydmMiOiJwYW5kb3JhIiwicHJvZHVjdCI6InNtYXJ0bGluayIsInNob3J0SWQiOiJob3BlbGVzc25lc3Mtb2YtbG92ZSIsImlzQXV0aG9yaXphdGlvblJlcXVpcmVkIjpmYWxzZSwib3duZXIiOiI1YzUwYjUxZDE0MDAwMDE5MDA2ODY4OTEiLCJ0ZW5hbnQiOiI1ZDJjMjk2M2YwZDUxZWViZDI0ZTc3ODciLCJhciI6IjVjYTNiODcyMGUwMDAwMGJhMzliYzRlMiIsImlzU2hvcnRMaW5rIjpmYWxzZX0'
-				/>
-				<Social
-					icon={insta}
-					link='https://www.instagram.com/waitingforsmith/?hl=en'
-					buttonText='Follow'
-					height='h-8'
-					width='w-1/6'
-				/>
+          <Social
+            icon="https://cloudinary-cdn.ffm.to/s--abCrNs3k--/h_64,c_scale/f_webp/https%3A%2F%2Fassets.ffm.to%2Fimages%2Flogo%2Fmusic-service_pandora.png"
+            link="https://api.ffm.to/sl/e/c/hopelessness-of-love?cd=eyJ1YSI6eyJ1YSI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXIiOnsibmFtZSI6IkNocm9tZSIsInZlcnNpb24iOiIxMTIuMC4wLjAiLCJtYWpvciI6IjExMiJ9LCJlbmdpbmUiOnsibmFtZSI6IkJsaW5rIiwidmVyc2lvbiI6IjExMi4wLjAuMCJ9LCJvcyI6eyJuYW1lIjoiTWFjIE9TIiwidmVyc2lvbiI6IjEwLjE1LjcifSwiZGV2aWNlIjp7fSwiY3B1Ijp7fX0sImNsaWVudCI6eyJyaWQiOiJkMmFhYTY0ZC04ZWE4LTRhNzQtYmQ4YS01Mjg3Mzg2NWI0MWYiLCJzaWQiOiJiMTg5MTVkMi00N2E4LTQ1ZDUtOGY1MS0yMWQ2MmRiOTZmZjgiLCJpcCI6IjE3OC44NS42Ni4xNTMiLCJyZWYiOiJodHRwczovL2wuZmFjZWJvb2suY29tLyIsImhvc3QiOiJkaXR0by5mbSIsImxhbmciOiJlbi1VUyIsImlwQ291bnRyeSI6Ik5MIn0sImlzV2VicFN1cHBvcnRlZCI6dHJ1ZSwiaXNGcm9tRVUiOnRydWUsImNvdW50cnlDb2RlIjoiTkwiLCJ1c2VBZmYiOiJvcmlnaW4iLCJpZCI6IjYwNzBmOGQxNDUwMDAwMTMwMDU2MGQ0ZiIsInBydiI6ZmFsc2UsImlzUHJlUiI6ZmFsc2UsInR6byI6bnVsbCwiY2giOm51bGwsImFuIjpudWxsLCJkZXN0VXJsIjoiaHR0cHM6Ly9wYW5kb3JhLmFwcC5saW5rLz8kZGVza3RvcF91cmw9aHR0cHMlM0ElMkYlMkZ3d3cucGFuZG9yYS5jb20lMkZhcnRpc3QlMkZ3YWl0aW5nLWZvci1zbWl0aCUyRmxpbmVzLW9mLWxvdmUtc2luZ2xlJTJGQUw1S2xmaDduNzUyNHBjJiRpb3NfZGVlcGxpbmtfcGF0aD1wYW5kb3JhdjQlM0ElMkYlMkZiYWNrc3RhZ2UlMkZhbGJ1bSUzRnRva2VuJTNEQUwlM0E1NTg5MDA2JiRhbmRyb2lkX2RlZXBsaW5rX3BhdGg9cGFuZG9yYXY0JTNBJTJGJTJGYmFja3N0YWdlJTJGYWxidW0lM0Z0b2tlbiUzREFMJTNBNTU4OTAwNiZ-Y2hhbm5lbD1QYXJ0bmVyJTIwQ2F0YWxvZyUyMFNlYXJjaCUyMEFQSSIsInZpZCI6ImI1YWFkMDJjLTM1NmMtNDMwYi1iZDY4LWJhMTdjODUyZGU3MSIsInNydmMiOiJwYW5kb3JhIiwicHJvZHVjdCI6InNtYXJ0bGluayIsInNob3J0SWQiOiJob3BlbGVzc25lc3Mtb2YtbG92ZSIsImlzQXV0aG9yaXphdGlvblJlcXVpcmVkIjpmYWxzZSwib3duZXIiOiI1YzUwYjUxZDE0MDAwMDE5MDA2ODY4OTEiLCJ0ZW5hbnQiOiI1ZDJjMjk2M2YwZDUxZWViZDI0ZTc3ODciLCJhciI6IjVjYTNiODcyMGUwMDAwMGJhMzliYzRlMiIsImlzU2hvcnRMaW5rIjpmYWxzZX0"
+          />
 
-				<Social
-					icon={web}
-					link='https://www.waitingforsmith.co.uk/'
-					buttonText='Explore'
-					height='h-8'
-					width='w-1/6'
-				/>
+          <Social
+            icon={insta}
+            link="https://www.instagram.com/waitingforsmith/?hl=en"
+            buttonText="Follow"
+            height="h-10"
+            width="w-12"
+          />
 
-			</div>
-		</section>
-	);
+          <Social
+            icon={web}
+            link="https://www.waitingforsmith.co.uk/"
+            buttonText="Explore"
+            height="h-10"
+            width="w-12"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Social = ({
-	icon,
-	link,
-	buttonText = 'Listen',
-	height = 'h-auto',
-	width = 'w-2/6',
+  icon,
+  link,
+  buttonText = "Listen",
+  height = "h-auto",
+  width = "w-2/6",
 }) => {
-	const imageScaling = `${height} ${width} w-2/6 object-scale-down`;
+  const imageScaling = `${height} ${width} object-scale-down`;
 
-	return (
-		<div className='flex w-full justify-between items-center border-b-2 border-slate-200 p-3 bg-slate-50'>
-			<img className={imageScaling} src={icon} alt='' />
-			<button className='bg-transparent flex justify-center items-center h-8 w-24 hover:bg-slate-500 text-slate-700 hover:text-white py-2 px-4 border border-slate-300 hover:border-transparent rounded'>
-				<a href={link} alt='' target='_blank' rel='noreferrer'>
-					{buttonText}
-				</a>
-			</button>
-		</div>
-	);
+  return (
+    <div className="flex items-center justify-between w-full border-b border-slate-200 p-4 hover:bg-slate-100 transition ease-in-out duration-300">
+      <img
+        className={`${imageScaling} w-1/5`}
+        src={icon}
+        alt="Social Icon"
+        onError={(e) => (e.target.src = "https://via.placeholder.com/64")}
+      />
+      <a href={link} target="_blank" rel="noreferrer">
+        <button className="bg-transparent flex justify-center items-center h-10 w-28 hover:bg-slate-500 hover:text-white text-slate-700 py-2 px-4 border border-slate-300 hover:border-transparent rounded-full transition ease-in-out duration-300">
+          {buttonText}
+        </button>
+      </a>
+    </div>
+  );
 };
 
 export default Social;
